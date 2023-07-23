6f8ccd346e5bc70a75ea3bac2d5998e6e93bbf3d
mahbubhswe <mahbubhswe@gmail.com>
2023-07-23 21:02:52 +0600

    9

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -48,19 +48,19 @@ export default function Navbar() {
                 sx={{ color: "#FFFFFF" }}
                 onClick={() => router.push("/udichi")}
               >
-                উদীচী
+                {/* উদীচী */}
               </Button>
               <Button
                 sx={{ color: "#FFFFFF" }}
                 onClick={() => router.push("/organization")}
               >
-                সংগঠন
+                {/* সংগঠন */}
               </Button>
               <Button
                 sx={{ color: "#FFFFFF" }}
                 onClick={() => router.push("/central-conferrance")}
               >
-                জাতীয় সম্মেলন
+                {/* জাতীয় সম্মেলন */}
               </Button>
               <Button
                 sx={{ color: "#FFFFFF" }}
@@ -71,12 +71,14 @@ export default function Navbar() {
               <Button
                 sx={{ color: "#FFFFFF" }}
                 onClick={() => router.push("/structure-resolution")}
-              ></Button>
+              >
+                গঠন তন্ত্র
+              </Button>
               <Button
                 sx={{ color: "#FFFFFF" }}
                 onClick={() => router.push("/next-day-program")}
               >
-                আগামী দিনের কর্মসূচি
+                {/* আগামী দিনের কর্মসূচি */}
               </Button>
             </Stack>
             <Typography flexGrow={1} />
